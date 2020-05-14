1c65b3651ab3c2f29e163c320c97d4352f7f8cbc
Leandro Dias <leandrodsdias@hotmail.com>
2020-05-14 02:30:14 -0300

    iniciando um  api de pagamentos para fins de estudos

---
+++ config/custon_express.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const consign = require('consign')
+
+
+module.exports = ()=>{
+
+
+    let app = express();
+
+        consign()
+            .include('routes')
+            .into(app)
+            
+    return app;
+};
---
+++ routes/payments.js
@@ -0,0 +1,8 @@
+
+module.exports = app =>{
+
+    app.get('/payments',(req,res)=>{
+
+        res.send('alo mundo')
+    })
+}
