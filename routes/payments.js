0dafd4d6912430c55c720dfc2c7097d03531a1b9
Leandro Dias <leandrodsdias@hotmail.com>
2020-05-16 02:46:43 -0300

    adicionado o  env exemplo  e mais duas rotas.

--- routes/payments.js
+++ routes/payments.js
@@ -1,6 +1,10 @@
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 
-
+const paymentBase = (app) => {
+    let connection = app.persistence.connectionFactory();
+    let paymentDao = new app.persistence.PaymentDao(connection);
+    return paymentDao
+};
 
 module.exports = app => {
 
@@ -11,20 +15,61 @@ module.exports = app => {
     app.get('/payments', (req, res) => {
 
 
-        let connection = app.persistence.connectionFactory();
-        let paymentDao = new app.persistence.PaymentDao(connection);
+        let paymentDao = paymentBase(app);
 
 
         paymentDao.list((error, success) => {
-            console.log('pagamentos .....');
             res.json(success)
             return;
         });
     });
 
+    app.put('/payments/payment/:id', (req, res) => {
+        let payment = {};
+        let paymentId = req.params.id;
+        let paymentDao = paymentBase(app);
+
+        payment.id = paymentId;
+        payment.status = PAYMENT_CONFIRMED;
+
+        paymentDao.update(payment, (error) => {
+            if (error) {
+                console.log('an error occurred while confirming the payment')
+                req.status(500).json(error);
+                return;
+            }
+            console.log('payment confirmed')
+            res.status(201).send(payment);
+            return;
+
+        })
+
+    });
+
+    app.delete('/payments/payment/:id', (req, res) => {
+        let payment = {};
+        let paymentId = req.params.id;
+        let paymentDao = paymentBase(app);
+
+        payment.id = paymentId;
+        payment.status = PAYMENT_CANCELED;
+
+        paymentDao.update(payment, (error) => {
+            if (error) {
+                console.log('an error occurred while canceling payment')
+                req.status(500).json(error);
+                return;
+            }
+            console.log('payment canceled successfully')
+            res.status(204).send(payment)
+            return;
+        })
+
+    });
+
     app.post('/payments/payment', [
         check("form_of_payment", 'payment method is mandatory').not().isEmpty(),
-        check("value", 'value not informed and must be a decimal value').not().isEmpty(),
+        check("value", 'value not informed and must be a decimal value').not().isEmpty().isFloat(),
         check("currency", 'Currency not reported').not().isEmpty(),
         check("description", "description not informed").not().isEmpty(),
     ], (req, res) => {
@@ -38,12 +83,11 @@ module.exports = app => {
         let payment = req.body;
 
         console.log("processando uma requisição de um novo pamaneto")
-        payment.status = 'pagamento gerado com sucesso';
+        payment.status = PAYMENT_CREATED;
         payment.date = new Date;
 
-        let connection = app.persistence.connectionFactory();
-        let paymentDao = new app.persistence.PaymentDao(connection);
 
+        let paymentDao = paymentBase(app);
 
         paymentDao.save(payment, (error, success) => {
 
